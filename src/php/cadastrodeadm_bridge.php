c3fd65ba37aead23fc4391f6521cb7048ab97b2e
Vitor Gomes <zbro2018@gmail.com>
2025-06-06 18:03:34 -0300

    bug fix

--- src/php/cadastrodeadm_bridge.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Include session validation
-include 'verificar_sessao.php';
-
-// Get admin info for personalization
-$admin_nome = isset($_SESSION['admin_usuario']) ? $_SESSION['admin_usuario'] : 'Administrador';
-?>
--- src/php/painel_bridge.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include 'verificar_sessao.php';
-
-
-$admin_nome = isset($_SESSION['admin_usuario']) ? $_SESSION['admin_usuario'] : 'Administrador';
-?>
